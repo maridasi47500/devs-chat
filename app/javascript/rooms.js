d6df73512f19b9709c3ab48911566b175e69055a
CJeanne ANonym <anonyme201504@gmail.com>
2023-06-14 09:51:18 -0300

    ok

--- app/javascript/rooms.js
+++ app/javascript/rooms.js
@@ -26,10 +26,11 @@ window.onload=function(){	// Get the modal
 	var textareas=document.getElementsByTagName("textarea");
 	for (var i=0;i<textareas.length;i++){
 	textareas[i].addEventListener('keydown', function(e) {
+		var start,end;
 		  if (e.key == 'Tab') {
 			      e.preventDefault();
-			      var start = this.selectionStart;
-			      var end = this.selectionEnd;
+			      start = this.selectionStart;
+			      end = this.selectionEnd;
 
 			      // set textarea value to: text before caret + tab + text after caret
 			       this.value = this.value.substring(0, start) +
